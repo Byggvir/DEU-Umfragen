cbb541e98596d5795f82eb9de65bbc731672d974
Thomas Arend <thomas@arend-rhb.de>
2022-08-21 23:49:47 +0200

    Switched to ggplot; script based on plot removed

--- R/Wahlumfragen.r
+++ R/Wahlumfragen.r
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 options(OutDec=',')
+
 MyScriptName <- "Wahlumfragen"
 
 require(data.table)
@@ -19,6 +20,9 @@ library(RCurl)
 library(rlist)
 library(stringr)
 library(argparser)
+
+#library(extrafont)
+#extrafont::loadfonts()
 
 # Set Working directory to git root
 
@@ -41,6 +45,8 @@ setwd(WD)
 source("R/lib/copyright.r")
 source("R/lib/sql.r")
 
+Institute <- RunSQL('select * from Institute;')
+
 today <- Sys.Date()
 heute <- format(today, "%Y%m%d")
 
@@ -53,164 +59,155 @@ if (length(args) == 0) {
   FromDay <- as.Date(args[1])
 }
 
-Institute <- data.frame(
-  Name = c(
-      'Allensbach'
-    , 'Infratest dimap'
-    , 'Kantar (Emnid)'
-    , 'Forsa'
-    , 'Forschungsgruppe Wahlen'
-    , 'GMS (Gesellschaft für Markt- und Sozialforschung)'
-    , 'INSA'
-    , 'YouGov'
-    )
-  , url = c(
-      'https://www.wahlrecht.de/umfragen/allensbach.htm'
-    , 'https://www.wahlrecht.de/umfragen/dimap.htm'
-    , 'https://www.wahlrecht.de/umfragen/emnid.htm'
-    , 'https://www.wahlrecht.de/umfragen/forsa.htm'
-    , 'https://www.wahlrecht.de/umfragen/politbarometer.htm'
-    , 'https://www.wahlrecht.de/umfragen/gms.htm'
-    , 'https://www.wahlrecht.de/umfragen/insa.htm'
-    , 'https://www.wahlrecht.de/umfragen/yougov.htm'
-    )
-)
+outdir <- 'png/Umfragen/'
+
+dir.create( outdir , showWarnings = TRUE, recursive = FALSE, mode = "0777")
+
+citation <- paste( '© Thomas Arend, 2022\nQuelle: © wahlrecht.de/umfragen\nStand', heute)
 
 ParteiFarbe <- data.frame(
-  Name = c('AfD', 'CDU_CSU', 'LINKE', 'FDP', 'FW','GRÜNE', 'PDS', 'PIRATEN', 'REP', 'SPD', 'SSW', 'Sonstige')
-  , Color = c('brown', 'black', 'purple', 'yellow', 'cyan', 'green', 'purple', 'grey', 'brown', 'red', 'blue', 'orange')
+  Name = c(    'CDU/CSU', 'SPD', 'FDP',    'FW',   'GRÜNE', 'LINKE',  'PDS',    'PIRATEN', 'REP',   'SSW',  'AfD',   'Sonstige')
+  , Color = c( 'black',   'red', 'yellow', 'cyan', 'green', 'purple', 'purple', 'grey',    'brown', 'blue', 'brown', 'orange'  )
 )
 
-for (INo in 1:nrow(Institute)) {
+umfragen <- RunSQL('select * from Umfragen;')
+umfragen$Partei <- factor( umfragen$Partei, levels = ParteiFarbe$Name, labels = ParteiFarbe$Name) 
 
-  print (Institute$Name[INo])
+umfragen$Institut <- factor( umfragen$Institut, levels = Institute$Name, labels = Institute$Shortname) 
+
+for (I in unique(umfragen$Institut) ) {
   
-  png( paste( 
-      "png/"
-    , MyScriptName
-    , '_'
-    , Institute$Name[INo]
-    , '.png'
-    , sep = ""
-    )
-    , width = 1980
-    , height = 1080
+  umfragen %>% filter( Institut == I ) %>% ggplot(
+    aes ( x = Datum, y = Ergebnis, colour = Partei )
+    ) + 
+    geom_smooth( aes(fill = Partei), method = 'glm' ) + 
+    geom_line( aes(colour = Partei)) +
+    geom_point( data = umfragen %>% filter( is.na(Befragte) & Institut == I ), size = 3 )+
+    geom_hline(yintercept = 0.05, color = 'red' , linetype = 'dotted') +
+    scale_x_date( date_labels = "%Y" ) +
+    scale_y_continuous( labels = scales::percent ) +
+    scale_color_manual( values = ParteiFarbe$Color, breaks = ParteiFarbe$Name) +
+    expand_limits( y = 0 ) +
+    facet_wrap(vars(Partei)) +
+    theme_ipsum() +
+    labs(  title = paste( "Umfragen und Wahlergebnisse Bundestag" )
+           , subtitle = paste( I )
+           , colour  = "Partei"
+           , x = "Datum"
+           , y = "Ergebnis"
+           , caption = citation )  -> PI
+  
+  ggsave(   filename = paste( outdir
+                             , 'Institut_'
+                             , I
+                             , '.png'
+                             , sep='')
+            , plot = PI
+            , device = "png"
+            , bg = "white"
+            , width = 29.1
+            , height = 21
+            , units = "cm"
   )
-  par( mar = c(10,5,5,5))
- 
-  HTML <- getURL(Institute$url[INo],.opts = list(ssl.verifypeer = FALSE), .encoding = 'UTF-8' )
-
-  tables <- readHTMLTable(HTML)
-
-  umfragen <- tables[[2]]
-  umfragen[,2] <- NULL
-  namen <- colnames(umfragen)
-  namen[1] <- 'Datum'
-  namen[2] <- 'CDU_CSU'
-  colnames(umfragen) <- namen
-  NoParteien <- match('Sonstige',namen)
-
-  umfragen[,NoParteien+1] <- NULL
-  umfragen[,1] <- as.Date(umfragen[,1],"%d.%m.%Y")
-  
-  namen <- colnames(umfragen)
-  
-  for (i in 2:(NoParteien-1)) {
-    umfragen[,i] <- as.numeric(str_replace(str_replace(str_replace_all(umfragen[,i]," %", ""),",","."),'(–-?)',"0"))
-  }
-
-  umfragen[,NoParteien] <- 100 - rowSums(umfragen[,2:(NoParteien-1)])
-  umfragen$Befragte[umfragen$Befragte=='Bundestagswahl'] <- 60000000
-
-  umfragen$Befragte <- as.numeric(str_remove(str_remove(umfragen$Befragte, '.* '), '\\.'))
-  
-  col <- ParteiFarbe$Color[match(namen[2:NoParteien],ParteiFarbe$Name)]
-  print(col)
-  
-  if (! is.na(FromDay) ) {
-    umfragen <- umfragen[umfragen$Datum >= FromDay,]
-  }
-  
-  plot( 
-      umfragen[,1]
-    , rep(NA,nrow(umfragen))
-    , type='l'
-    , lwd = 5
-    , col = col[1]
-    , main = Institute$Name[INo]
-    , sub = ''
-    , xlab = 'Datum'
-    , ylab = 'Anteil [%]'
-    , cex.main = 4
-    , cex.sub = 1
-    , ylim = c(0,60)
-  )
-
-  title ( 
-      sub = 'Wenn am kommenden Sonntag Bundestagswahl wäre ...'
-    , line = 5
-    , cex.sub = 3
-  )
-
-  legend( 
-      'topright'
-    , title = ' Parteien'
-    , legend = str_replace_all(colnames(umfragen[,2:NoParteien]),'_','/')
-    , lty = 1
-    , lwd = 2
-    , col = col
-    , cex = 2
-    , inset = 0.05
-
-  )
-  legend( 
-    'topleft'
-    , title = 'Bundestagswahlen'
-    , legend = 'Wahlergebnis'
-    , pch = 19
-    , lty = 1
-    , lwd = 2
-    , col = 'black'
-    , cex = 2
-    , inset = 0.05
-  )
-  
-
-  for ( i in 2:NoParteien ) {
-      lines(
-        umfragen[,1]
-      , umfragen[,i]
-      , type='l'
-      , lwd = 5
-      , col = col[i-1]
-    )
-
-    points(
-        umfragen[umfragen$Befragte==60000000,1]
-      , umfragen[umfragen$Befragte==60000000,i]
-      , type = 'p'
-      , pch = 19
-      , col = col[i-1]
-      , lwd = 20
-    )
-  
-    f <- as.formula(paste( namen[i], "~", "Datum"))
-  
-    ra <- lm( f, data = umfragen)
-    print(namen[i])
-    print(ra$coefficients[1]+as.numeric(today)*ra$coefficients[2])
-    
-    abline( 
-        coef = ra$coefficients
-      , col = col[i-1]
-      , lty = 2
-      , lwd = 5
-    )
-  
-  }
-
-  grid()
-
-  dev.off()
 
 }
+
+for (P in unique(umfragen$Partei ) ) {
+
+  umfragen %>% filter( Partei == P & Datum > as.Date('2021-12-31') ) %>% ggplot(
+    aes ( x = Datum, y = Ergebnis )
+  ) +
+    geom_smooth( aes(fill = Partei), method = 'glm' ) +
+    geom_line( aes(colour = Partei)) +
+    geom_hline(yintercept = 0.05, color = 'red' , linetype = 'dotted') +
+    geom_point( data = umfragen %>% filter( is.na(Befragte) & Partei == P ), size = 3 )+
+    scale_x_date( date_labels = "%Y" ) +
+    scale_y_continuous( labels = scales::percent ) +
+    scale_color_manual( values = ParteiFarbe$Color, breaks = ParteiFarbe$Name) +
+    expand_limits( y = 0 ) +
+    facet_wrap(vars(Institut)) +
+    theme_ipsum() +
+    labs(  title = paste( "Umfragen und Wahlergebnisse Bundestag" )
+           , subtitle = paste( P )
+           , colour  = "Partei"
+           , x = "Datum"
+           , y = "Ergebnis"
+           , caption = citation )  -> PT
+
+  ggsave(   filename = paste( outdir
+                              , 'Partei_'
+                              , str_replace(P, '/' , '_' )
+                              , '-S.png'
+                              , sep='')
+            , plot = PT
+            , device = "png"
+            , bg = "white"
+            , width = 29.1
+            , height = 21
+            , units = "cm"
+  )
+  
+  umfragen %>% filter( Partei == P ) %>% ggplot(
+    aes ( x = Datum, y = Ergebnis )
+  ) +
+    geom_smooth( aes(fill = Partei), method = 'glm' ) +
+    geom_line( aes(colour = Partei)) +
+    geom_hline(yintercept = 0.05, color = 'red' , linetype = 'dotted') +
+    geom_point( data = umfragen %>% filter( is.na(Befragte) & Partei == P ), size = 3 )+
+    scale_x_date( date_labels = "%Y" ) +
+    scale_y_continuous( labels = scales::percent ) +
+    scale_color_manual( values = ParteiFarbe$Color, breaks = ParteiFarbe$Name) +
+    expand_limits( y = 0 ) +
+    facet_wrap(vars(Institut)) +
+    theme_ipsum() +
+    labs(  title = paste( "Umfragen und Wahlergebnisse Bundestag" )
+           , subtitle = paste( P )
+           , colour  = "Partei"
+           , x = "Datum"
+           , y = "Ergebnis"
+           , caption = citation )  -> PT
+  
+  ggsave(   filename = paste( outdir
+                              , 'Partei_'
+                              , str_replace(P, '/' , '_' )
+                              , '-L.png'
+                              , sep='')
+            , plot = PT
+            , device = "png"
+            , bg = "white"
+            , width = 29.1
+            , height = 21
+            , units = "cm"
+  )
+  
+}
+
+  
+  umfragen %>% filter( ! is.na(Befragte) )  %>% ggplot(
+    aes ( x = Institut, y = Befragte )
+  ) +
+    geom_boxplot(  ) +
+    expand_limits( y = 0 ) +
+    theme_ipsum() +
+    theme (
+      axis.text.x = element_text(angle = 90)
+    ) +
+    labs(  title = paste( "Befragte nach Institut" )
+           , subtitle = ''
+           , colour  = "Institut"
+           , x = "Institut"
+           , y = "Befragte"
+           , caption = citation )  -> IB
+  
+  ggsave(   filename = paste( outdir
+                              , 'Befragte' 
+                              , '.png'
+                              , sep='')
+            , plot = IB
+            , device = "png"
+            , bg = "white"
+            , width = 29.1
+            , height = 21
+            , units = "cm"
+  )
+  
